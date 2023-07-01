1c00c296ddfe27a6bb2023288d208b0bd4ce39bc
hosseinTabaie <hosseinkazemi7896@gmail.com>
2023-07-01 03:35:48 +0330

    overwrite django models for my project

--- app/models.py
+++ app/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager as BUM
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+# Overwrite Django models --------------------------------
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseUserManager(BUM):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password,
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+    username_validator = UnicodeUsernameValidator()
+
+    email = models.EmailField(verbose_name="email address", unique=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = BaseUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def is_staff(self):
+        return self.is_admin
+
+# -------------------------------------------------------------------------
--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
@@ -100,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL = 'app.BaseUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
